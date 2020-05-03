ebd26045190657e8870a592be5286e505fbec054
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-04 02:41:12 +0300

    logout

---
+++ InstagramClone/InstagramClone/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  InstagramClone
+//
+//  Created by Büşra Serdaroğlu on 4.05.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        performSegue(withIdentifier: "goLogin", sender: nil)
+    }
+}
